caf324a4e4e40d78049c1550b258d14e07affe72
Brooks MacLachlan <brooksel_14@hotmail.com>
2017-02-26 21:49:06 -0500

    Users can now supply a text file defining the board they want to solve

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 This program solves Gyul! Hap! puzzles as seen on the Korean game show The Genius.
 
-Currently, the program only functions in GHCI, and the Gyul! Hap! boards must be user-defined and supplied to the findHaps function. The boards are defined as a list of tuples, where each tuple describes a picture on the board. For example, the tuple (Red, Circle, Black) describes a picture of a red circle on a black background.
+You will need a text file containing information about the Gyul! Hap! board you want to solve. Each line of the text file should contain the colour, shape, and background colour, in that order and seprated by spaces, for a picture on your board. The information for each picture on the board should be on a different line. 
+
+You can see how your file should be formatted by looking at the "exampleInput.txt" file.
+
+To run the program from a command line, navigate to your directory containing the "gyulhap.hs" file as well as your input file, and execute the following command:
+
+cat exampleInput.txt | runhaskell gyulhap
+
+Of course, you should replace "exampleInput.txt" with the name of your input file.
+
+The program can also be run in GHCI. For that, the Gyul! Hap! boards must be user-defined and supplied to the findHaps function. The boards are defined as a list of tuples, where each tuple describes a picture on the board. For example, the tuple (Red, Circle, Black) describes a picture of a red circle on a black background.
--- gyulhap.hs
+++ gyulhap.hs
@@ -1,14 +1,22 @@
-data Colour = Red | Blue | Yellow deriving (Show, Eq)
+import Data.List
 
-data Shape = Circle | Square | Triangle deriving (Show, Eq)
+data Colour = Red | Blue | Yellow deriving (Show, Eq, Read)
 
-data Background = Black | Grey | White deriving (Show, Eq)
+data Shape = Circle | Square | Triangle deriving (Show, Eq, Read)
+
+data Background = Black | Grey | White deriving (Show, Eq, Read)
 
 type Tile = (Colour, Shape, Background)
 
 type Board = [Tile]
 
 type Hap = (Tile, Tile, Tile)
+
+main = do
+    contents <- getContents
+    let inputBoard = map readTuple $ map listToTuple $ map words $ lines contents
+        solutionHaps = intercalate "\n\n" $ map (intercalate ", ") $ map (mapThreeTuple showTuple) $ findHaps inputBoard
+    putStrLn $ "The haps are:\n\n" ++ solutionHaps
 
 trialBoard :: Board
 trialBoard = [(Yellow, Triangle, White), (Blue, Square, White), (Yellow, Circle, Grey), (Blue, Triangle, Black), (Red, Circle, White), (Yellow, Square, White), (Yellow, Triangle, Grey), (Blue, Circle, Black), (Red, Square, Black)]
@@ -20,22 +28,22 @@ findHaps grid =
         trues = filter snd (zip tileSet evals)
         sol = [fst x | x <- trues]
     in sol
-	
+
 splitTriads :: Board -> [Hap]
 splitTriads board = removeDups [(t1, t2, t3) | t1 <- board, t2 <- board, t3 <- board, t1 /= t2, t1 /= t3, t2 /= t3]
 
 evalHap :: Hap -> Bool
 evalHap hap
-	| propertyCondition getColour hap && propertyCondition getShape hap && propertyCondition getBackground hap = True
-	| otherwise = False
-	
+    | propertyCondition getColour hap && propertyCondition getShape hap && propertyCondition getBackground hap = True
+    | otherwise = False
+
 propertyCondition :: (Eq a) => (Tile -> a) -> Hap -> Bool
 propertyCondition f hap
-	| x == y, y == z, x == z = True
-	| x /= y, y /= z, x /= z = True
-	| otherwise              = False
-	where (x,y,z) = mapHap f hap
-	
+    | x == y, y == z, x == z = True
+    | x /= y, y /= z, x /= z = True
+    | otherwise              = False
+    where (x,y,z) = mapHap f hap
+
 makeHap :: Tile -> Tile -> Tile -> Hap
 makeHap tile1 tile2 tile3 = (tile1, tile2, tile3)
 
@@ -63,12 +71,24 @@ thirdTile (t1, t2, t3) = t3
 removeDups :: [Hap] -> [Hap]
 removeDups [] = []
 removeDups (h:xs)
-	| (x,z,y) `elem` xs = removeDups xs
-	| (y,x,z) `elem` xs = removeDups xs
-	| (y,z,x) `elem` xs = removeDups xs
-	| (z,x,y) `elem` xs = removeDups xs
-	| (z,y,x) `elem` xs = removeDups xs
-	| otherwise         = h:removeDups xs
-	where x = fstTile h
-	      y = sndTile h
-	      z = thirdTile h
+    | (x,z,y) `elem` xs = removeDups xs
+    | (y,x,z) `elem` xs = removeDups xs
+    | (y,z,x) `elem` xs = removeDups xs
+    | (z,x,y) `elem` xs = removeDups xs
+    | (z,y,x) `elem` xs = removeDups xs
+    | otherwise         = h:removeDups xs
+    where x = fstTile h
+          y = sndTile h
+          z = thirdTile h
+
+listToTuple :: [a] -> (a, a, a)
+listToTuple [p1, p2, p3] = (p1, p2, p3)
+
+readTuple :: (Read a, Read b, Read c) => (String, String, String) -> (a, b, c)
+readTuple (colour, shape, background) = (read colour, read shape, read background)
+
+showTuple :: (Show a, Show b, Show c) => (a, b, c) -> String
+showTuple (colour, shape, background) = show colour ++ " " ++ show shape ++ " " ++ show background
+
+mapThreeTuple :: (a -> b) -> (a, a, a) -> [b]
+mapThreeTuple f (tile1, tile2, tile3) = [f tile1, f tile2, f tile3]
