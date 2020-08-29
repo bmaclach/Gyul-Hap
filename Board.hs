de71640fe9a1f7e04bebde384a4080d0ea6fad7c
Brooks MacLachlan <brooksel_14@hotmail.com>
2020-08-28 21:51:11 -0400

    Boy was this hideous. Refactor to be less horror-inducing.

--- Board.hs
+++ Board.hs
@@ -1,40 +1,62 @@
 module Board where
 
-data Colour = Red | Blue | Yellow | Purple | Green | Orange deriving (Show, Eq, Read)
+import Data.List
+import qualified Data.Set as Set
 
-data Shape = Circle | Square | Triangle | Sun | Moon | Star deriving (Show, Eq, Read)
+data Colour = Red | Blue | Yellow | Purple | Green | Orange deriving (Show, Eq, Read, Ord)
 
-data Background = Black | Grey | White deriving (Show, Eq, Read)
+readColour :: String -> Colour
+readColour "R" = Red
+readColour "B" = Blue
+readColour "Y" = Yellow
+readColour "P" = Purple
+readColour "G" = Green
+readColour "O" = Orange
+readColour c = read c
 
-type Tile = (Colour, Shape, Background)
+data Shape = Circle | Square | Triangle | Sun | Moon | Star deriving (Show, Eq, Read, Ord)
+
+readShape :: String -> Shape
+readShape "C" = Circle
+readShape "Sq" = Square
+readShape "T" = Triangle
+readShape "S" = Sun
+readShape "M" = Moon
+readShape "St" = Star
+readShape s = read s
+
+data Background = Black | Grey | White deriving (Show, Eq, Read, Ord)
+
+readBackground :: String -> Background
+readBackground "B" = Black
+readBackground "Bk" = Black
+readBackground "G" = Grey
+readBackground "Gy" = Grey
+readBackground "W" = White
+readBackground bg = read bg
+
+data Tile = Tile {
+  colour :: Colour, 
+  shape :: Shape,
+  background :: Background} deriving (Eq, Ord)
+
+instance Show Tile where
+  show (Tile c s b) = show c ++ " " ++ show s ++ " " ++ show b
+
+instance Read Tile where
+  readsPrec _ str = readTile $ words str
+    where readTile (c:s:b:rest) = [(Tile (readColour c) (readShape s) (readBackground b), unwords rest)]
+          readTile _ = error $ "Each tile in the input file should be on a " ++
+            "new line and described by the colour, shape, and background, " ++
+            "separated by spaces"
 
 type Board = [Tile]
 
-type Hap = (Tile, Tile, Tile)
+data Hap = Hap Tile Tile Tile
 
-trialBoard :: Board
-trialBoard = [
-  (Yellow, Triangle, White), 
-  (Blue, Square, White), 
-  (Yellow, Circle, Grey), 
-  (Blue, Triangle, Black), 
-  (Red, Circle, White), 
-  (Yellow, Square, White), 
-  (Yellow, Triangle, Grey), 
-  (Blue, Circle, Black), 
-  (Red, Square, Black)]
+instance Show Hap where
+  show (Hap t1 t2 t3) = intercalate ", " $ map show [t1, t2, t3]
 
-makeHap :: Tile -> Tile -> Tile -> Hap
-makeHap tile1 tile2 tile3 = (tile1, tile2, tile3)
-
-mapHap :: (Tile -> a) -> Hap -> (a, a, a)
-mapHap f (t1, t2, t3) = (f t1, f t2, f t3)
-
-getColour :: Tile -> Colour
-getColour (c,s,b) = c
-
-getShape :: Tile -> Shape
-getShape (c,s,b) = s
-
-getBackground :: Tile -> Background
-getBackground (c,s,b) = b
+instance Eq Hap where
+  (Hap t11 t12 t13) == (Hap t21 t22 t23) = 
+    Set.fromList [t11, t12, t13] == Set.fromList [t21, t22, t23]
--- gyulhap.hs
+++ gyulhap.hs
@@ -1,16 +1,13 @@
 import Data.List
 
 import Board
-import Helpers
 
 main = do
-    contents <- getContents
-    let inputBoard = map readTuple $ map listToTuple $ (map . map) decodeShortForm $ map words $ filter (not . null) $ lines contents
-        solutionHaps = intercalate "\n\n" $ map (intercalate ", ") $ map (mapThreeTuple showTuple) $ findHaps inputBoard
-    putStrLn $ serveOutputText solutionHaps
-
-validInput :: [String]
-validInput = ["Red", "Blue", "Yellow", "Purple", "Green", "Orange", "Circle", "Square", "Triangle", "Sun", "Moon", "Star", "Black", "Grey", "White"]
+  contents <- getContents
+  let inputBoard :: Board
+      inputBoard = map read $ filter (not . null) $ lines contents
+      solutionHaps = findHaps inputBoard
+  putStrLn $ serveOutputText solutionHaps
 
 findHaps :: Board -> [Hap]
 findHaps [] = error "Your input file is empty!"
@@ -18,46 +15,24 @@ findHaps [a] = error "Your input file has only one picture. You need at least th
 findHaps [a, b] = error "Your input file has only two pictures. You need at least three. Please update your input file."
 findHaps grid =
     let tileSet = splitTriads grid
-        evals = map evalHap tileSet
+        evals = map isHap tileSet
         trues = filter snd (zip tileSet evals)
         sol = [fst x | x <- trues]
     in sol
 
 splitTriads :: Board -> [Hap]
-splitTriads board = removeDups [(t1, t2, t3) | t1 <- board, t2 <- board, t3 <- board, t1 /= t2, t1 /= t3, t2 /= t3]
+splitTriads board = nub [Hap t1 t2 t3 | t1 <- board, t2 <- board, t3 <- board, t1 /= t2, t1 /= t3, t2 /= t3]
 
-evalHap :: Hap -> Bool
-evalHap hap
-    | propertyCondition getColour hap && propertyCondition getShape hap && propertyCondition getBackground hap = True
-    | otherwise = False
+isHap :: Hap -> Bool
+isHap (Hap t1 t2 t3) = let tiles = [t1, t2, t3] 
+  in allSameOrDifferent colour tiles 
+  && allSameOrDifferent shape tiles
+  && allSameOrDifferent background tiles
 
-propertyCondition :: (Eq a) => (Tile -> a) -> Hap -> Bool
-propertyCondition f hap
-    | x == y, y == z, x == z = True
-    | x /= y, y /= z, x /= z = True
-    | otherwise              = False
-    where (x,y,z) = mapHap f hap
+allSameOrDifferent :: (Eq a) => (Tile -> a) -> [Tile] -> Bool
+allSameOrDifferent f es = let numDistincts = length $ nub (map f es) 
+    in numDistincts == 1 || numDistincts == length es
 
-decodeShortForm :: String -> String
-decodeShortForm a
-    | isInfixOf [a] validInput = a
-    | a == "R" = "Red"
-    | a == "B" = "Blue"
-    | a == "Y" = "Yellow"
-    | a == "P" = "Purple"
-    | a == "G" = "Green"
-    | a == "O" = "Orange"
-    | a == "C" = "Circle"
-    | a == "Sq" = "Square"
-    | a == "T" = "Triangle"
-    | a == "S" = "Sun"
-    | a == "M" = "Moon"
-    | a == "St" = "Star"
-    | a == "Bk" = "Black"
-    | a == "Gy" = "Grey"
-    | a == "W" = "White"
-    | otherwise = error "There's something in your input file that I can't read! Please double-check the formatting and try again."
-
-serveOutputText :: String -> String
-serveOutputText "" = "\nThere are zero haps on this board!"
-serveOutputText (haps) = "\nThe haps are:\n\n" ++ haps
+serveOutputText :: [Hap] -> String
+serveOutputText [] = "\nThere are zero haps on this board!"
+serveOutputText haps = intercalate "\n\n" $ "\nThe haps are:" : map show haps
